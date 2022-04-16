9a57f696dc71a431d51a6163ea0e336bf2fc0ba8
jkj89507 <JKJ89507@gmail.com>
2022-04-16 05:33:02 +0300

    Add check auth without switch button, need to fix

--- main.py
+++ main.py
@@ -19,15 +19,50 @@ def myCaptcaha():
     mainApp.resizable(False, False)
     windowPositionCenter(mainApp)
 
+    def login():
+        amountImagesNeedTouch = 0
+
+        keyFile = open("key.txt", "r")
+        getKey = keyFile.readline().split(" ")
+        keyFile.close()
+        nameForChoose = []
+        imagesTouchedPath = []
+        for i in arrayOfListButtons:
+            nameForChoose.append(i.getIndex())
+            print(nameForChoose[len(nameForChoose) - 1])
+            if i. == True:
+                imagesTouchedPath.append(i.getIndex())
+
+        for i in range(0, len(getKey)):
+            for j in range(0, len(nameForChoose)):
+                if getKey[i] == nameForChoose[j]:
+                    amountImagesNeedTouch += 1
+        print(amountImagesNeedTouch)
+
+        if len(imagesTouchedPath) != amountImagesNeedTouch:
+            messagebox.showwarning("Access abort", "Auth is not success!")
+            exit(0)
+            mainApp.destroy()
+        elif amountImagesNeedTouch != 0:
+            for i in imagesTouchedPath:
+                if i not in getKey:
+                    messagebox.showwarning("Access abort", "Auth is not success!")
+                    exit(0)
+        else:
+            messagebox.showinfo("Access denied", "Auth is success! Welcome!")
+            mainApp.destroy()
+
+
     xCoord = 10
     yCoord = 110
     arrayOfListButtons = []
     useImages = []
 
     mainLabel = Label(mainApp, text= "Выберите элементы в \nсоответствии с ключем\nбезопасности:",
-                      anchor=tkinter.W,  font="Arial 20 bold",
+                      anchor=tkinter.W,  font="Batang 20 bold",
                       bg="#1992fc", fg="#fcfcfc",
-                      justify=tkinter.LEFT, width=25, height=3)
+                      justify=tkinter.LEFT, width=25,
+                      height=3)
     mainLabel.place(x=12, y=5)
 
     for i in range(0, 16):
@@ -61,6 +96,16 @@ def myCaptcaha():
         if xCoord >= 440:
             yCoord += 110
             xCoord = 10
+
+    checkLoginBtn = myBtn(mainApp)
+    checkLoginBtn["font"] = "Batang 14 bold"
+    checkLoginBtn["text"] = "Войти"
+    checkLoginBtn["bg"] = "#1992fc"
+    checkLoginBtn["activebackground"] = "#fcfcfc"
+    checkLoginBtn["fg"] = "#fcfcfc"
+    checkLoginBtn["activeforeground"] = "#1992fc"
+    checkLoginBtn["command"] = lambda: login()
+    checkLoginBtn.place(x=370, y=550)
 
     mainApp.mainloop()
 
